412413f26419afcafa2f36bf995830cbcf385378
julialiu16 <49254790+julialiu16@users.noreply.github.com>
2019-10-31 17:40:37 -0700

    node js server

---
+++ website.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+  
+var path = __dirname + '/MDB/';
+  
+app.use('/',router);
+  
+router.get('/',function(req, res){
+  res.sendFile(path + 'index.html');
+});
+  
+router.get('/home',function(req, res){
+  res.sendFile(path + 'home.html');});
+  
+router.get('/login',function(req, res){
+  res.sendFile(path + 'login.html');
+});
+
+router.get('/forgot',function(req, res){
+    res.sendFile(path + 'forgotpw.html');
+});
+  
+app.use('*',function(req, res){
+  res.send('Error 404: Not Found!');
+});
+  
+app.listen(3000,function(){
+  console.log("Server running at Port 3000");
+});
