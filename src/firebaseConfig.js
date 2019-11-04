f3d177fde2131e7e55a8ab2ce4791a16b6416bbe
Junayed Naushad <junayednaushad@gmail.com>
2019-11-04 11:09:59 -0800

    added firebase authorization

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCri31YveE6WMCjfA9UMJH7-NdVU1ZJbu8",
+    authDomain: "payjunction-4be6a.firebaseapp.com",
+    databaseURL: "https://payjunction-4be6a.firebaseio.com",
+    projectId: "payjunction-4be6a",
+    storageBucket: "payjunction-4be6a.appspot.com",
+    messagingSenderId: "199914665361",
+  };
+
+export default firebaseConfig;
