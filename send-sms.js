f08b0802ef17f3730f8ef2c433c72303f85f16e9
Junayed Naushad <junayednaushad@gmail.com>
2019-11-14 17:50:06 -0800

    added twilio feature to send text message - Junayed, Howard

---
+++ send-sms.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'AC0be21a36ad616556b63e4fb2de58b74f';
+const authToken = '7aed3e119c852ce96f3276611ea3bcbf';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'Payment request',
+     from: '+13213237705',
+     to: '+19498702089'
+   })
+  .then(message => console.log(message.sid));
