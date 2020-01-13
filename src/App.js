4c59c306f61cc1b60c5c8f9c233b2f4a85d87d6c
julialiu16 <julia.zihan.liu@gmail.com>
2020-01-12 22:10:22 -0800

    jsx not compiling

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+class App extends Comment{
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route>
+
+            </Route>
+          </Switch>
+        </Router>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
