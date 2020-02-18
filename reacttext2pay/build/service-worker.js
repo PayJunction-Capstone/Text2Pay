3cd39e88b1dc17c49997be53f5da2e06b8b72dcc
julialiu16 <julia.zihan.liu@gmail.com>
2020-02-18 09:02:43 -0800

    trial

--- reacttext2pay/build/service-worker.js
+++ reacttext2pay/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b033636b47e509c1968e4513cdac4cd.js"
+  "/precache-manifest.87a38b9dc8b6055b983561876144c133.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
