701281bd89761941c8585f5a126a6d3b5ff504bb
Joanne Li <joanneli@Joannes-MacBook-Pro.local>
2020-03-06 20:43:53 -0800

    added recurring option

---
+++ build/precache-manifest.3111b51f33567611651d00ceff212746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935883f5a1cc69d821ed7b8af01b7ba8",
+    "url": "/PayJunction-Capstone/Text2Pay/index.html"
+  },
+  {
+    "revision": "8a5019a2e8f3ad5d5215",
+    "url": "/PayJunction-Capstone/Text2Pay/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ee2b5e1d0a98baadd71b",
+    "url": "/PayJunction-Capstone/Text2Pay/static/js/2.a57fee4b.chunk.js"
+  },
+  {
+    "revision": "b5c8281ca89c1ddb89c15ead0e00f32f",
+    "url": "/PayJunction-Capstone/Text2Pay/static/js/2.a57fee4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a5019a2e8f3ad5d5215",
+    "url": "/PayJunction-Capstone/Text2Pay/static/js/main.a3ac7c82.chunk.js"
+  },
+  {
+    "revision": "621a9c4434ff2a4e2319",
+    "url": "/PayJunction-Capstone/Text2Pay/static/js/runtime-main.0221bda2.js"
+  }
+]);
--- reacttext2pay/build/service-worker.js
+++ reacttext2pay/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1008fc97b787c57c70561b85b0670260.js"
+  "/precache-manifest.89b1be401077036bfc76162fb612f092.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
